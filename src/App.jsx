1147d468efd472c8ab92e8be31ea5d7ed5b57c20
Your Name <sarajitmondal012@gmail.com>
2024-04-26 09:58:15 +0600

    navbar design complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/header/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className='bg-red-500'>This is myname</h1>
+    <div className='flex flex-col justify-between min-h-screen'>
+    <header className=' '>
+     <NavBar></NavBar>
+    </header>
+    <main className='w-11/12 mx-auto max-w-[1280px]'>
+      <Outlet></Outlet>
+    </main>
+    <footer>
+      <h1>This is out footer</h1>
+    </footer>
     </div>
   )
 }
