ef2c28be4cb994ca47d0601de3120c26c298eaae
Your Name <sarajitmondal012@gmail.com>
2024-04-30 20:17:46 +0600

    function complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,12 @@ function Home() {
 
   return (
     <div className='mb-10'>
-    
+
       <h1 className='text-3xl font-bold mt-10 mb-8 text-center  underline'><span className='underline text-sky-500 font-extrabold'>Craft</span> items section</h1>
       <CreftItemSection></CreftItemSection>
       <h1 className='text-3xl text-center  mt-16 mb-8  font-bold my-12 underline'><span className='underline text-sky-500 font-extrabold'>Art & Craft</span>  Categories Section</h1>
       <AllCategory></AllCategory>
-      <h1 className='text-3xl text-center  mt-20 mb-8  font-bold  underline'><span className='underline text-sky-500 font-extrabold'>Our Services </span>  
+      <h1 className='text-3xl text-center  mt-20 mb-8  font-bold  underline'><span className='underline text-sky-500 font-extrabold'>Our Services </span>
         Include Creative,Digital,
         Video,<span className='underline text-sky-500 font-extrabold'>Paint Artist</span>Gallery</h1>
       <OurServices></OurServices>
